259fef212d518d95e6e26af1bb0ad7c305ac41eb
Angela Moreno <angela.moreno.cubillos@gmail.com>
2023-06-05 22:18:15 -0500

    Creacion Carpetas ejercicios

---
+++ Clases/4.1.1_funciones/script.js
@@ -0,0 +1,32 @@
+// // Deberas realizar un semáforo utilizando JS.
+// // El semáforo deberá cambiar de luz de Verde a Amarilla y de Amarilla a Roja y luego de nuevo a Verde.
+// // Puedes mostrar la imagen del semáforo cambiando, hacer el cambio con console.log o con alert.
+// // Se debe respetar el orden de los colores:
+// // De rojo pasa a Verde.
+// // De amarillo puede pasar a Rojo.
+// // De Verde pasa a Amarillo.
+
+// Podrías intentar con algo como :
+// 1
+// document.write(“<img src=’http://imagenes.com/luz_verde.png’ >”);
+
+// function imprimeHola() {
+
+//             console.log('Hola'); 
+
+//         }
+
+        
+
+//         function detente(intervalo) {
+
+//             clearInterval(intervalo);
+
+//             console.log('Listo!');
+
+//         }
+
+//                 var intervalo = setInterval(imprimeHola(),1000); 
+
+//         setTimeout(detener(intervalo),5000); 
+//         Puedes usar este proyecto de referencia: https://replit.com/@danyparc/semaforo#index.html.
